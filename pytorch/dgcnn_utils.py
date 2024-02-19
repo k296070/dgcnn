b692fc68e3340a4e98cf4c30753be73bd81b6a8c
HOJOONKIM <k296070@snu.ac.kr>
2024-02-19 18:10:53 +0900

    Update dgcnn_utils.py

--- pytorch/dgcnn_utils.py
+++ pytorch/dgcnn_utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch.cuda.nvtx as nvtx
 
 #def print(*args):
 #    return
@@ -13,7 +14,7 @@ def knn(x, k=20):
 
 
 def get_graph_feature(x, k=20, idx=None):
-    #nvtx.range_push("ggf")
+    nvtx.range_push("ggf")
     batch_size = x.size(0)
     num_points = x.size(2)
     x = x.view(batch_size, -1, num_points)
@@ -21,9 +22,9 @@ def get_graph_feature(x, k=20, idx=None):
         idx = knn(x, k=k)   # (batch_size, num_points, k)
     device = torch.device('cuda')
 
-    #idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1)*num_points
+    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1)*num_points
 
-    idx_base = torch.arange(0, batch_size).view(-1, 1, 1)*num_points
+    #idx_base = torch.arange(0, batch_size).view(-1, 1, 1)*num_points
 
     idx = idx + idx_base
 
@@ -37,13 +38,13 @@ def get_graph_feature(x, k=20, idx=None):
     x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
     
     feature = torch.cat((x,feature-x), dim=3).permute(0, 3, 1, 2).contiguous()
-    #nvtx.range_pop()
+    nvtx.range_pop()
   
     return feature
 
 def get_graph_feature_DA(x, k=20, idx=None):
 
-#    nvtx.range_push("ggf")    
+    nvtx.range_push("ggf")    
     batch_size = x.size(0)
     num_points = x.size(2)
     x = x.view(batch_size, -1, num_points)
@@ -58,7 +59,7 @@ def get_graph_feature_DA(x, k=20, idx=None):
     num_points = point_cloud_shape[2]
     num_dims = point_cloud_shape[1]
 
-    idx_ = torch.arange(batch_size) * num_points
+    idx_ = torch.arange(batch_size, device=device) * num_points
 
     idx_ = idx_.view(batch_size, 1, 1)
 
@@ -71,5 +72,5 @@ def get_graph_feature_DA(x, k=20, idx=None):
     x = x.view(batch_size, num_points, 1, num_dims)
     
     feature = torch.cat((x,point_cloud_neighbors-x), dim=3).permute(0, 3, 1, 2).contiguous()
-#    nvtx.range_pop()
+    nvtx.range_pop()
     return feature
