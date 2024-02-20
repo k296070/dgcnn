6f7e11b0452db2ff611807ef722366eefb357c4d
HOJOONKIM <k296070@snu.ac.kr>
2024-02-20 14:17:02 +0900

    Update dgcnn_utils.py

--- pytorch/dgcnn_utils.py
+++ pytorch/dgcnn_utils.py
@@ -70,32 +70,31 @@ def get_graph_feature_DA(x, k=20, idx=None):
     nvtx.range_push("ggf")    
     batch_size = x.size(0)
     num_points = x.size(2)
-    print("input",x.shape)
     x = x.view(batch_size, -1, num_points)
     if idx is None:
         idx = knn(x, k=k)   # (batch_size, num_points, k)
     device = torch.device('cuda')
-
-    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1)*num_points
-    #idx_base = torch.arange(0, batch_size).view(-1, 1, 1)*num_points
-    idx = idx + idx_base
-
-    idx = idx.view(-1)
-
-    num_dims = x.size(1)
-    print("after view",x.shape)
-    x = x.transpose(2, 1).contiguous()   # (batch_size, num_points, num_dims)  -> (batch_size*num_points, num_dims) #   batch_size * num_points * k + range(0, batch_size*num_points)
-    feature = x.view(batch_size*num_points, -1)[idx, :]
     
-    print("feature",feature.shape)
-    feature = feature.view(batch_size, num_points, k, num_dims) 
-    feature = feature.permute(0, 3, 1, 2)
-    feature = feature.max(dim= -1, keepdim=True)[0]
-    feature = feature.permute(0, 2, 3, 1)
-    print("view before",x.shape)
-    x = x.view(batch_size, num_points, 1, num_dims)
-    print("after view",x.shape)
-    feature = torch.cat((x,feature-x), dim=3).permute(0, 3, 1, 2).contiguous()
-    nvtx.range_pop()
+    point_cloud_shape = x.size()
+    batch_size = point_cloud_shape[0]
+    num_points = point_cloud_shape[2]
+    num_dims = point_cloud_shape[1]
 
+    #idx_ = torch.arange(batch_size) * num_points
+    idx_ = torch.arange(batch_size, device=device) * num_points
+
+    idx_ = idx_.view(batch_size, 1, 1)
+
+    point_cloud_flat = x.reshape(-1, num_dims)
+
+    point_cloud_neighbors = point_cloud_flat[idx + idx_]
+    print("!!!!! shape", point_cloud_neighbors.shape)
+    point_cloud_neighbors = point_cloud_neighbors.permute(0, 3, 1, 2)
+    point_cloud_neighbors = torch.max(point_cloud_neighbors, dim=-1, keepdim=True)[0]
+    point_cloud_neighbors = point_cloud_neighbors.permute(0, 2, 3, 1)
+    
+    x = x.reshape(batch_size, num_points, 1, num_dims)
+    
+    feature = torch.cat((x,point_cloud_neighbors-x), dim=3).permute(0, 3, 1, 2).contiguous()
+    nvtx.range_pop()
     return feature
